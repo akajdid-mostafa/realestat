8910b92420b5d95e665107dee30673f4448cccdd
ocean connecting <zaaranaouali@gmail.com>
2024-09-08 00:17:42 +0100

    dss

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
       return [
         {
           source: '/login',
-          destination: '/Index',
+          destination: '/',
           permanent: true,
         },
       ];
